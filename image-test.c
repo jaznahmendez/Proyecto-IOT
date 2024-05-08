f29a397b34b5126e08a99542d5a5f92be25af26f
Jazmin Mendez <jazmin.mendez@iteso.mx>
2024-05-07 19:06:13 -0600

    image test color

--- image-test.c
+++ image-test.c
@@ -13,6 +13,7 @@ struct wl_display *display;
 struct wl_compositor *compositor;
 struct wl_shm *shm;
 struct wl_shell *shell;
+struct wl_surface *surface;
 
 /*Global Handlers*/
 void registry_global_handler(void *data, struct wl_registry *registry, 
@@ -72,65 +73,33 @@ int main(int argc, char *argv[]) {
         return -1;
     }
 
-    struct wl_surface *surface = wl_compositor_create_surface(compositor);
+    surface = wl_compositor_create_surface(compositor);
     struct wl_shell_surface *shell_surface = wl_shell_get_shell_surface(shell, surface);
     wl_shell_surface_set_toplevel(shell_surface);
 
-    // Load JPEG image
-    FILE *jpg_file = fopen("image.jpg", "rb");
-    if (!jpg_file) {
-        printf("Error opening image file\n");
-        return -1;
-    }
+    // Set background color (solid color)
+    uint32_t color = 0xFF0000FF; // ARGB format (Alpha, Red, Green, Blue)
 
-    struct jpeg_decompress_struct cinfo;
-    struct jpeg_error_mgr jerr;
+    // Create shared memory pool and buffer for solid color
+    int stride = 4; // 4 bytes per pixel for ARGB8888 format
+    int width = 1920; // Width of the screen
+    int height = 1080; // Height of the screen
+    int size = stride * width * height; // Size of the buffer
 
-    cinfo.err = jpeg_std_error(&jerr);
-    jpeg_create_decompress(&cinfo);
-    jpeg_stdio_src(&cinfo, jpg_file);
-    jpeg_read_header(&cinfo, TRUE);
-    jpeg_start_decompress(&cinfo);
-
-    int width = cinfo.output_width;
-    int height = cinfo.output_height;
-    int num_channels = cinfo.output_components; // Number of color channels
-
-    // Calculate stride (bytes per row) based on the width and number of channels
-    int stride = width * num_channels;
-    // Ensure stride is aligned to 4 bytes (32 bits)
-    stride = (stride + 3) & ~3;
-
-    int size = stride * height;
-    unsigned char *raw_image_data = malloc(size);
-
-    // Read scanlines one by one and copy them into raw_image_data buffer
-    unsigned char *row_pointer = raw_image_data;
-    while (cinfo.output_scanline < cinfo.output_height) {
-        row_pointer += jpeg_read_scanlines(&cinfo, &row_pointer, 1) * stride;
-    }
-
-    jpeg_finish_decompress(&cinfo);
-    jpeg_destroy_decompress(&cinfo);
-    fclose(jpg_file);
-
-    // Create shared memory pool and buffer
     int fd = syscall(SYS_memfd_create, "buffer", 0);
     ftruncate(fd, size);
     unsigned int *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    memcpy(data, raw_image_data, size);
+    memset(data, color, size); // Fill buffer with the solid color
 
     struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
     struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool,
-        0, width, height, stride, WL_SHM_FORMAT_ARGB8888);
+        0, width, height, stride * 8, WL_SHM_FORMAT_ARGB8888);
 
     wl_surface_attach(surface, buffer, 0, 0);
     wl_surface_commit(surface);
 
-    free(raw_image_data);
-
-    while (1) {
-        wl_display_dispatch(display);
+    while (wl_display_dispatch(display) != -1) {
+        // Wait for the compositor to close the surface
     }
 
     wl_display_disconnect(display);
