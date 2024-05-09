65731db747dda44a3f856b5f4c2d3853b148e076
Jazmin Mendez <jazmin.mendez@iteso.mx>
2024-05-08 18:49:26 -0600

    final?

--- test.c
+++ test.c
@@ -9,21 +9,36 @@
 #include <json-c/json.h>
 #include <wayland-client.h>
 
-
 struct wl_display *display;
 struct wl_compositor *compositor;
-struct wl_surface *surface;
+struct wl_shm *shm;
+struct wl_shell *shell;
 
 void registry_global_handler(void *data, struct wl_registry *registry, 
-                             uint32_t id, const char *interface, uint32_t version) {
+        uint32_t id, const char *interface, uint32_t version) {
+
+    printf("interface %s, version %d, id: %d\n", interface, version, id);
+
     if (strcmp(interface, "wl_compositor") == 0) {
         compositor = wl_registry_bind(registry, id, &wl_compositor_interface, 1);
+    } else if (strcmp(interface, "wl_shm") == 0) {
+        shm = wl_registry_bind(registry, id, &wl_shm_interface, 1);
+    } else if (strcmp(interface, "wl_shell") == 0) {
+        shell = wl_registry_bind(registry, id, &wl_shell_interface, 1);
     }
 }
 
+void registry_global_remove_handler(void *data, struct wl_registry *registry, 
+        uint32_t id) {
 
-struct wl_registry_listener registry_listener = {
-    registry_global_handler
+    printf("Removed %d\n", id);
+}
+
+struct wl_registry *registry;
+
+struct wl_registry_listener listener = {
+    registry_global_handler,
+    registry_global_remove_handler
 };
 
 
@@ -51,6 +66,83 @@ char* base64_encode(const unsigned char *data, size_t input_length) {
     }
 
     return encoded_data;
+}
+
+void display_image(){
+    display = wl_display_connect(NULL);
+    registry = wl_display_get_registry(display);
+    wl_registry_add_listener(registry, &listener, NULL);
+
+    wl_display_dispatch(display);
+    wl_display_roundtrip(display);
+
+    struct wl_surface *surface = wl_compositor_create_surface(compositor);
+    struct wl_shell_surface *shell_surface = wl_shell_get_shell_surface(shell, surface);
+    wl_shell_surface_set_toplevel(shell_surface);
+
+    FILE *jpg_file = fopen("image.jpg", "rb");
+    if (!jpg_file) {
+        printf("Error opening image file\n");
+        return -1;
+    }
+
+    struct jpeg_decompress_struct cinfo;
+    struct jpeg_error_mgr jerr;
+
+    cinfo.err = jpeg_std_error(&jerr);
+    jpeg_create_decompress(&cinfo);
+    jpeg_stdio_src(&cinfo, jpg_file);
+    jpeg_read_header(&cinfo, TRUE);
+    jpeg_start_decompress(&cinfo);
+
+    int width = cinfo.output_width;
+    int height = cinfo.output_height;
+    int num_channels = cinfo.output_components; 
+
+    int stride = width * 4; 
+    stride = (stride + 3) & ~3;
+
+    int size = stride * height;
+    unsigned char *raw_image_data = malloc(size);
+
+    unsigned char *row_pointer = raw_image_data;
+    while (cinfo.output_scanline < cinfo.output_height) {
+        jpeg_read_scanlines(&cinfo, &row_pointer, 1);
+        row_pointer += stride;
+    }
+
+    jpeg_finish_decompress(&cinfo);
+    jpeg_destroy_decompress(&cinfo);
+    fclose(jpg_file);
+
+    int fd = syscall(SYS_memfd_create, "buffer", 0);
+    ftruncate(fd, size);
+    unsigned int *data = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    
+    for (int i = 0; i < height; i++) {
+        for (int j = 0; j < width; j++) {
+            unsigned char *src_pixel = &raw_image_data[i * stride + j * num_channels];
+            unsigned int *dst_pixel = &data[i * width + j];
+            *dst_pixel = 0xFF000000 | (src_pixel[0] << 16) | (src_pixel[1] << 8) | src_pixel[2];
+        }
+    }
+
+    struct wl_shm_pool *pool = wl_shm_create_pool(shm, fd, size);
+    struct wl_buffer *buffer = wl_shm_pool_create_buffer(pool,
+        0, width, height, stride, WL_SHM_FORMAT_ARGB8888);
+
+    wl_surface_attach(surface, buffer, 0, 0);
+    wl_surface_commit(surface);
+
+    free(raw_image_data);
+
+    while (1) {
+        wl_display_dispatch(display);
+    }
+
+    wl_display_disconnect(display);
+
+    return 0;
 }
 
 size_t write_callback(void *ptr, size_t size, size_t nmemb, struct json_object *json_obj) {
@@ -93,7 +185,7 @@ void api_call(const char *base64_string) {
         return;
     }
 
-     struct json_object * metadata;
+     truct json_object * metadata;
     struct json_object * track;
     struct json_object * title;
     struct json_object * subtitle;
@@ -116,6 +208,9 @@ void api_call(const char *base64_string) {
     //printf("Response JSON:\n%s\n", json_object_to_json_string_ext(response_json, JSON_C_TO_STRING_PRETTY));
 
     json_object_put(response_json);
+
+    display_image();
+    return;
 }
 
 
