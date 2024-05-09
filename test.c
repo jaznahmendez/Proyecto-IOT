1a4a77ecb4e7ae5a9fc6ebc8196d4c18cefadee6
Jazmin Mendez <jazmin.mendez@iteso.mx>
2024-05-08 19:15:31 -0600

    final??

--- test.c
+++ test.c
@@ -221,7 +221,7 @@ void api_call(const char *base64_string) {
 
 
 int main() {
-    FILE *file = fopen("salmini.raw", "rb");
+    FILE *file = fopen("For Whom The Bell Tolls (Remastered).raw", "rb");
     if (file == NULL) {
         perror("Error opening file");
         return 1;
